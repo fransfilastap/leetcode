bf4cef0353477e681608311bf1ca39a0f02085db
fransfilastap@live.com <fransfilastap@Live.com>
2022-12-21 11:13:30 +0700

    refactor: the problem description forbid using string

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,19 +1,27 @@
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        String s = x+"";
-        StringBuilder builder = new StringBuilder();
-        for (int i=s.length()-1;i>=0;i--){
-            builder.append(s.charAt(i));
+        if((x < 0 || x % 10==0) && x != 0) return false;
+
+        return reverseNumber(x)==x;
+
+    }
+
+    public int reverseNumber(int x){
+        int number = x,remainder=0,reversed=0;
+
+        while (number != 0){
+            remainder = number%10;
+            reversed = reversed*10;
+            reversed = reversed + remainder;
+            number = number /10;
         }
 
-        return s.compareTo(builder.toString())==0;
-
+        return reversed;
     }
 
     public static void main(String ...args){
 
-        int[] inputs = new int[]{121,-121,10,1112111};
+        int[] inputs = new int[]{121,-121,10,1112111,0};
         PalindromeNumber palindromeNumber = new PalindromeNumber();
         for (int input:inputs) {
             System.out.println("Input ["+input+"] is palindrom ? "+palindromeNumber.isPalindrome(input));
